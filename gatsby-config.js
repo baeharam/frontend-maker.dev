0d653063d2d8a34ef4deb6ed5416641b64639a3e
mazino <mazino@spoqa.com>
2021-03-21 17:15:28 +0900

    구글 애널리틱스 ID 추가
    Signed-off-by: mazino <mazino@spoqa.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-R21R0Z6VBS`,
       },
     },
     `gatsby-plugin-feed`,
